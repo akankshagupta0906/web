955f5b73703f8ce9bc0b3ad98f81b505bfcedffc
akanksha503gupta <83533236+akanksha504gupta@users.noreply.github.com>
2021-07-30 09:49:48 -0400

    webdaity

--- WebDaity/settings.py
+++ WebDaity/settings.py
@@ -132,6 +132,6 @@ USE_TZ = True
 MEDIA_URL='/media/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[STATIC_DIR]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
